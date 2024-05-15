3e72f7111cfc0bf40339df8281b06c1a3239388d
Michael F. Herbst <info@michael-herbst.com>
2024-05-16 00:05:00 +0200

    Updates

--- 0_generate_structures.jl
+++ 0_generate_structures.jl
@@ -1,11 +1,16 @@
+#!/bin/sh
+#=
+BN=$(basename "$0" .jl)
+julia --project -t1 $BN.jl
+exit $?
+=#
 using AtomsBuilder
 using AtomsIO
 using AtomsBase
 
-systems = []
-for repeat in 4:4
+systems = AbstractSystem[]
+for repeat in 3:3
     system = bulk(:Al, cubic=true) * (repeat, repeat, repeat)
-    push!(systems, system)
-    # push!(systems, rattle!(FlexibleSystem(system)), 1e-4)
+    push!(systems, rattle!(FlexibleSystem(system), 1e-3))
 end
 save_trajectory("Al_bulk.extxyz", systems)
--- run_calculations.jl
+++ run_calculations.jl
@@ -1,16 +1,18 @@
 #!/bin/sh
 #=
 BN=$(basename "$0" .jl)
-julia --project -t4 $BN.jl | tee $BN.log
+mpiexecjl --project -np 8 julia --project -t1 $BN.jl | tee $BN.log
 exit $?
 =#
 using AtomsIO
 using DFTK
+using LazyArtifacts
 setup_threading()
 
 function run_calculation(system)
-    @assert all(isequal(:Al), atomic_symbols(system))
+    @assert all(isequal(:Al), atomic_symbol(system))
 
+    system = attach_psp(system; Al=artifact"pd_nc_sr_pbe_stringent_0.4.1_upf/Al.upf")
     model  = model_PBE(system; smearing=Smearing.Gaussian(), temperature=1e-3)
     kgrid  = kgrid_from_maximal_spacing(model, 0.13)
     basis  = PlaneWaveBasis(model; Ecut=26, kgrid)
